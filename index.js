4e92638da3246b010d9ad55f081f403f07bbd649
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-06 22:01:37 -0500

    new content

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var enterCity = document.querySelector("#cities");
 var button = document.querySelector("#submitButton");
-var cityName = document.querySelector("#cities");
+var cityName = document.querySelector(".displayName");
+var weather = document.querySelector(".displayWeather");
+var desc = document.querySelector(".displayDesc");
+var longtitude = document.querySelector(".longitude");
+var latitdue = document.querySelector(".latitude");
 
 var apiKey = "&appid=1fe453e20e18fc07ec52f3bfdc6679a9";
+var getLatitdue;
+var getLong;
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
@@ -13,60 +19,17 @@ button.addEventListener("click", function (e) {
   )
     .then((response) => response.json())
 
-    .then((data) => console.log(data))
-
+    .then((data) => {
+      console.log(data);
+      var nameValue = data.city["name"];
+      console.log(nameValue);
+      var getLatitdue = data.city.coord.lat;
+      console.log(getLatitdue);
+      var getLong = data.city.coord.lon;
+      console.log(getLong);
+      return getLatitdue;
+    })
     .catch((error) => console.log(error));
 });
 
-// fetch(url + apiKey, {
-//   method: "GET",
-// })
-//   .then((response) => {
-//     console.log(response);
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     for (var i = 0; i < data.length; i++) {
-//       console.log(data);
-//     }
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
-// fetch(url + apiKey, {
-//   method: "GET",
-// })
-//   .then((response) => {
-//     console.log(response);
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     for (var i = 0; i < data.length; i++) {
-//       console.log(data.temp);
-//     }
-//     console.log(data.list.weather);
-//     document.innerHTML = data;
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
-// fetch("http://api.openweathermap.org/geo/1.0/direct?q=&appid=" + apiKey, {
-//   method: "GET",
-// })
-//   .then((response) => {
-//     console.log(response);
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     for (var i = 0; i < data.length; i++) {}
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
-//when user adds a city find the city in API
-// S
+console.log(getLatitdue);
