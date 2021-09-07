f1d51b936fdd917393c32a7ba50cc16863addcff
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-06 20:13:32 -0500

    added to find cities in the input

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
-var apiKey = "1fe453e20e18fc07ec52f3bfdc6679a9";
-var url =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hour,daily&appid=";
+var enterCity = document.querySelector("#cities");
+var button = document.querySelector("#submitButton");
+var cityName = document.querySelector("#cities");
 
-fetch(url + apiKey, {
-  method: "GET",
-})
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    for (var i = 0; i < data.length; i++) {
-      console.log();
-    }
-    console.log(data);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+var apiKey = "&appid=1fe453e20e18fc07ec52f3bfdc6679a9";
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      enterCity.value +
+      apiKey
+  )
+    .then((response) => response.json())
+
+    .then((data) => console.log(data))
+
+    .catch((error) => console.log(error));
+});
+
+// fetch(url + apiKey, {
+//   method: "GET",
+// })
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data);
+//     }
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+// fetch(url + apiKey, {
+//   method: "GET",
+// })
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data.temp);
+//     }
+//     console.log(data.list.weather);
+//     document.innerHTML = data;
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+// fetch("http://api.openweathermap.org/geo/1.0/direct?q=&appid=" + apiKey, {
+//   method: "GET",
+// })
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     for (var i = 0; i < data.length; i++) {}
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+//when user adds a city find the city in API
+// S
