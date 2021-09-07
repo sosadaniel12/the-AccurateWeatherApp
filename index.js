ad0b2fb28d8f23557f597722bbe37d5a6f04ba5e
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-06 22:50:51 -0500

    added innerHTMl

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var enterCity = document.querySelector("#cities");
 var button = document.querySelector("#submitButton");
 var cityName = document.querySelector(".displayName");
 var weather = document.querySelector(".displayWeather");
-var desc = document.querySelector(".displayDesc");
+var date = document.querySelector(".displayDate");
 var longtitude = document.querySelector(".longitude");
 var latitdue = document.querySelector(".latitude");
+var temperature = document.querySelector(".temperature");
+
 var key = "1fe453e20e18fc07ec52f3bfdc6679a9";
 var apiKey = "&appid=1fe453e20e18fc07ec52f3bfdc6679a9";
 var getLatitdue;
@@ -23,11 +25,23 @@ button.addEventListener("click", function (e) {
       console.log(data);
       var nameValue = data.city["name"];
       console.log(nameValue);
+
       var getLatitdue = data.city.coord.lat;
       console.log(getLatitdue);
       var getLong = data.city.coord.lon;
+
       var getName = data.city.name;
       cityName.innerHTML = getName;
+
+      var getDate = data.list[0].dt_txt;
+      date.innerHTML = getDate;
+
+      var getWeather = data.list[0].weather[0].icon;
+      weather.innerHTML = getWeather;
+
+      var getTemperature = data.list[0].main.temp;
+      temperature.innerHTML = getTemperature;
+
       fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${getLatitdue}&lon=${getLong}&appid=${key}`
       )
