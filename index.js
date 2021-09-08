3a3ec9b4eeb576de3b61171f149542bed7ff3a1d
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-07 20:09:58 -0500

    minor changes

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var date = document.querySelector("#displayDate");
 var longtitude = document.querySelector("#longitude");
 var latitdue = document.querySelector("#latitude");
 var temperature = document.querySelector("#temperature");
+var humdity = document.querySelector("#humdity");
+var windSpeed = document.querySelector("#windSpeed");
+var uvIndex = document.querySelector("#uvIndex");
 
 var key = "1fe453e20e18fc07ec52f3bfdc6679a9";
 var apiKey = "&appid=1fe453e20e18fc07ec52f3bfdc6679a9";
@@ -22,6 +25,9 @@ button.addEventListener("click", function (e) {
     .then((response) => response.json())
 
     .then((data) => {
+      var weatherBox = document.querySelector(".weatherBox");
+      weatherBox.style.border = "solid black 5px";
+
       console.log(data);
       var nameValue = data.city["name"];
       console.log(nameValue);
@@ -38,16 +44,32 @@ button.addEventListener("click", function (e) {
       date.innerHTML = getDate;
 
       var getWeather = data.list[0].weather[0].icon;
-      weather.innerHTML = getWeather;
+      // weather.innerHTML = getWeather;
 
-      var getTemperature = data.list[0].main.temp;
+      var getTemperature = data.list[0].main.temp + "\u00B0F";
       temperature.innerHTML = getTemperature;
+
+      var getHumdity = data.list[0].main.humidity + "%";
+      humdity.innerHTML = getHumdity;
+
+      var getWindSpeed = data.list[0].wind.speed + "mph";
+      windSpeed.innerHTML = getWindSpeed;
+
+      fetch(`https://openweathermap.org/img/w/${getWeather}.png`)
+        .then((response) => response.json())
+
+        .then((icon) => {
+          weather.innerHTML = getWeather;
+          console.log(icon);
+        });
 
       fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${getLatitdue}&lon=${getLong}&appid=${key}`
       )
         .then((response) => response.json())
         .then((info) => {
+          var getUvIndex = info.current.uvi + " UV INDEX";
+          uvIndex.innerHTML = getUvIndex;
           console.log(info);
         });
 
