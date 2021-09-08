d9b1926f922c1a0a54e2b05eb83d8390c42d52f1
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-07 19:05:24 -0500

    added local time

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var enterCity = document.querySelector("#cities");
 var button = document.querySelector("#submitButton");
-var cityName = document.querySelector(".displayName");
-var weather = document.querySelector(".displayWeather");
-var date = document.querySelector(".displayDate");
-var longtitude = document.querySelector(".longitude");
-var latitdue = document.querySelector(".latitude");
-var temperature = document.querySelector(".temperature");
+var cityName = document.querySelector("#displayName");
+var weather = document.querySelector("#displayWeather");
+var date = document.querySelector("#displayDate");
+var longtitude = document.querySelector("#longitude");
+var latitdue = document.querySelector("#latitude");
+var temperature = document.querySelector("#temperature");
 
 var key = "1fe453e20e18fc07ec52f3bfdc6679a9";
 var apiKey = "&appid=1fe453e20e18fc07ec52f3bfdc6679a9";
@@ -15,7 +15,7 @@ var getLong;
 button.addEventListener("click", function (e) {
   e.preventDefault();
   fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&q=" +
       enterCity.value +
       apiKey
   )
@@ -34,6 +34,7 @@ button.addEventListener("click", function (e) {
       cityName.innerHTML = getName;
 
       var getDate = data.list[0].dt_txt;
+      getDate = moment().format("L");
       date.innerHTML = getDate;
 
       var getWeather = data.list[0].weather[0].icon;
